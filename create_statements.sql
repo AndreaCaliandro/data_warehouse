021f88ced6b250f321a0014c03883f409690e03f
andrea caliandro <a.caliandro@samsung.com>
2019-01-06 22:18:40 -0800

    fixed foreign key

--- create_statements.sql
+++ create_statements.sql
@@ -18,22 +18,24 @@ CREATE TABLE Facility (
 
 CREATE TABLE Location (
     locno       varchar(8) primary key,
-    facno       varchar(8) foreign key references Facility,
+    facno       varchar(8) not null,
     locname     varchar(50) not null,
-    CONSTRAINT NNfacno NOT NULL (facno)
+    FOREIGN KEY (facno) REFERENCES Facility(facno)
 );
 
 CREATE TABLE EventRequest (
     eventno     varchar(8) primary key,
     dateheld    date not null,
     datereq     date not null,
-    facno       varchar(8) not null foreign key references Facility,
-    custno      varchar(8) not null foreign key references Customer,
+    facno       varchar(8) not null,
+    custno      varchar(8) not null,
     dateauth    date,
     status      varchar(50) not null,
     estcost     float not null,
     estaudience integer not null,
     budno       varchar(8),
+    FOREIGN KEY (facno) REFERENCES Facility(facno),
+    FOREIGN KEY (custno) REFERENCES Customer(custno),
     CHECK (status IN ('Pending', 'Denied', 'Approved')),
     CHECK (estaudience > 0)
 );
@@ -48,11 +50,13 @@ CREATE TABLE Employee(
 
 CREATE TABLE EventPlan (
     planno      varchar(8) primary key,
-    eventno     varchar(8)  not null foreign key references EventRequest,
+    eventno     varchar(8)  not null,
     workdate    date not null,
     notes       varchar(255),
     activity    varchar(50) not null,
-    empno       varchar(8) foreign key references Employee
+    empno       varchar(8),
+    FOREIGN KEY (eventno) REFERENCES EventRequest(eventno),
+    FOREIGN KEY (empno) REFERENCES Employee(empno)
 );
 
 CREATE TABLE ResourceTbl (
@@ -63,13 +67,16 @@ CREATE TABLE ResourceTbl (
 );
 
 CREATE TABLE EventPlanLine (
-    PlanNo varchar(8) not null foreign key references EventPlan,
+    PlanNo varchar(8) not null,
     LineNo varchar(8) not null,
     TimeStart datetime not null,
     TimeEnd datetime not null,
     NumberFld integer not null,
-    LocNo varchar(8) not null foreign key references Location,
-    ResNo varchar(8) not null foreign key references ResourceTbl,
+    LocNo varchar(8) not null,
+    ResNo varchar(8) not null,
     CONSTRAINT PK_EventPlanLine PRIMARY KEY (PlanNo, LineNo),
-    CHECK (TimeStart < TimeEnd)
+    CHECK (TimeStart < TimeEnd),
+    FOREIGN KEY (PlanNo) REFERENCES EventPlan(planno),
+    FOREIGN KEY (LocNo) REFERENCES Location(locno),
+    FOREIGN KEY (ResNo) REFERENCES ResourceTbl(resno)
 );
